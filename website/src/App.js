bd012d098b94b3c572ce050c8def21ccdac7ef92
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-04-26 21:19:42 -0700

    rendered header element

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Nav';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
      <Navigation />
+     <Header />
     </div>
   );
 }
