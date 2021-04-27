2b2920ca6695d5104333dd420cfa79ce2d0c25ec
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-04-26 21:20:04 -0700

    created header and css file for it

---
+++ website/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import logo from '../../logo.svg';
+import react from 'react';
+import  './Header.css';
+
+function Header() {
+  return (
+   <div className="header">
+     <h1>Hello, I am <span>Sahil Sharma</span>, Full-Stack Developer.</h1> 
+   </div>
+  );
+}
+
+export default Header;
