0ba48aa36283ef3d69166a4ce0f7db8cfaf3ae3e
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-04-26 18:29:27 -0700

    new component Navigation created

---
+++ website/src/components/Nav.js
@@ -0,0 +1,25 @@
+import logo from '../logo.svg';
+import react from 'react';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+
+function Navigation() {
+  return (
+    <>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Sahil Sharma</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">HOME</Nav.Link>
+      <Nav.Link href="#about">ABOUT</Nav.Link>
+      <Nav.Link href="#portfolio">POTFOLIO</Nav.Link>
+      <Nav.Link href="#contact">CONTACT</Nav.Link>
+    </Nav>
+   
+  </Navbar.Collapse>
+</Navbar>
+</>
+  );
+}
+
+export default Navigation;
