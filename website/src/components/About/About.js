b8343a09a28df6a50accc15264f40cfd5ff1cc5d
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-08-13 05:06:10 -0700

    completed about section

--- website/src/components/About/About.js
+++ website/src/components/About/About.js
@@ -1,40 +1,80 @@
 import logo from '../../logo.svg';
 import react from 'react';
 import './About.css';
-import Progress from '../Progress/Progress'
+import Progress from '../Progress/Progress';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLightbulb, faTachometerAlt, faLaptop, faRocket } from '@fortawesome/free-solid-svg-icons'
+
 
 function About() {
   return (
+    <>
+    <h1 className="header-about">ABOUT </h1>
+    <div className="icon-container">
+      <div className="icon-bar">
+         <div className="icon"><FontAwesomeIcon color="#4963f8" icon={faTachometerAlt} size='4x'/></div>
+         <h1 className='Font-header'>Fast</h1>
+         <p>Fast load times and lag free interaction, my highest priority.</p>
+      </div>
+      
+      <div className="icon-bar">
+         <div className="icon"><FontAwesomeIcon color="#4963f8" icon={faLightbulb} size='4x'/></div>
+         <h1 className="Font-header">Responsive</h1>
+         <p>Strong preference for easy to use, intuitive UX/UI.</p>
+      </div>
+      <div className="icon-bar">
+      <div className="icon"><FontAwesomeIcon color="#4963f8" icon={faLaptop} size='4x'/></div>
+      <h1 className="Font-header">Intuitive</h1>
+      <p>My layouts will work on any device, big or small.</p>
+      </div>
+      <div className="icon-bar">
+      <div className='icon'><FontAwesomeIcon color="#4963f8" icon={faRocket} size='4x'/></div>
+      <h1 className="Font-header">Dynamic</h1>
+      <p>Websites don't have to be static, I love making pages come to life.</p>
+      </div>
+      
+   </div>
     <section className="about">
     <div className="section-image">
       <div className="content-wrap">
-        <h4>Thunderstrike was born to fill in the gap for Motorcycle Gear for Canadian buyers and redefine the options that are available to the Canadian motorcycle rider.<br></br><br></br>
-
-        We are a Canadian company and are working to create motorcycle gear that is stylish and protective. We are trying to bridge the gap of most brands and websites showing products as unavailable. Pricing has been an important factor in our partner meetings and we have kept the prices affordable. With riding season being so short in Canada we will try out best to meet demands for our clients.<br></br><br></br>
-        
-        Our mission is to provide affordable but stylish Motorcycle Gear to Canadians. </h4>
+        <div className="image-container"> <img className="image" src="image.png" /></div>
+        <p>I am a Front-end developer based out of Surrey, BC, Canada. I have a serious passion for creating fast and responsive websites using the latest technologies.</p>
         </div>
     </div>
     <div className="section-skills">
-  
-        <div className="skills">
-          <Progress done="70" />
+    <div className="skills"> 
+        <div className="skill-name">JavaScript</div> <Progress done="90" />
         </div>
         <div className="skills">
-          <Progress done="50" />
-        </div>
-        <div className="skills"> 
-          <Progress done="90" />
+          <div className="skill-name">React</div><Progress done="70" />
         </div>
         <div className="skills">
-          <Progress done="70" />
+        <div className="skill-name">HTML</div> <Progress done="70" />
         </div>
         <div className="skills">
-          <Progress done="70" />
+        <div className="skill-name">CSS</div> <Progress done="70" />
+        
+        </div>
+        
+        <div className="skills">
+        <div className="skill-name">Node.js</div> <Progress done="60" />
+        
+        </div>
+        <div className="skills">
+        <div className="skill-name">Express.js</div> <Progress done="50" />
+        
+        </div>
+        <div className="skills">
+        <div className="skill-name">UI Design</div> <Progress done="50" />
+        
+        </div>
+        <div className="skills">
+        <div className="skill-name">Angular</div><Progress done="50" />
         </div>
       </div>
    
   </section>
+  </>
   );
 }
 
